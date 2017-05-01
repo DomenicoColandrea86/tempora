910608b0bce63e86a4a4ac0d8b067da4cd098da9
Domenico Colandrea <dcolandrea@rcanalytics.com>
2017-05-01 12:39:18 -0400

    updates android/app/build.gradle file, updates react-native versio to 0.44

--- app/screens/Root/index.js
+++ app/screens/Root/index.js
@@ -12,7 +12,7 @@ class RootContainer extends Component {
   render() {
     return (
       <View style={styles.applicationView}>
-        <StatusBar barStyle="light-content" />
+        <StatusBar />
         <Navigation />
       </View>
     );
